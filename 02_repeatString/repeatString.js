306b0006c79d48423b912f642bf577f3471ec4ef
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-22 16:48:34 +0000

    3 exercises passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedString, n) {
+    let returnString  = "";
+    if (n < 0)  {
+        return 'ERROR';
+    }
+    for(i=0; i<n; i++){
+        returnString += repeatedString;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let n =  string.length  -1;
+    let  reversedString  = "";
+    for ( n; n >=0 ; n--){
+        reversedString += `${string[n]}`;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
