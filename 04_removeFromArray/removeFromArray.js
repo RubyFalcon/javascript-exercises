6fb61103bce26b40445493333b6226a6a7c994b9
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-23 17:31:41 +0000

    create function using foreach array method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, n) {
+    let returnArray = [];
+    array.forEach( (item) => {
+        if (item != n){
+            returnArray.push(item);
+        } 
+    }
+
+    );
+        
+    return returnArray;
+    }
+   
 
 // Do not edit below this line
 module.exports = removeFromArray;
