ad35495517b17a3bbc9701c58918d1ba83f20ff6
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-24 21:44:45 +0000

    refactored sumAll function to return error when input is not a number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let finalSum = 0;
+    let i ;
+    let n;
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return "ERROR";
+    if (firstNum > secondNum) {
+        i = secondNum
+        n = firstNum
+    } else {
+        i = firstNum;
+        n = secondNum;
+    }
+    for (i; i <= n; i++) {
+        if( i < 0){
+            return "ERROR";
+        }
+        finalSum += i;
+    }
+    return finalSum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
