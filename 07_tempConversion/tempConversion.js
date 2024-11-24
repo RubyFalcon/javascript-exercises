0a6051019173d2fac3bbddedd7154a7e804a7bf8
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-24 22:27:22 +0000

    CREATE tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhenheitTemp) {
+  let celciusTemp = ((farhenheitTemp -32)* (5/9));
+  return Math.round((celciusTemp *10)) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let farhenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  return Math.round((farhenheitTemp * 10)) / 10;
 };
 
 // Do not edit below this line
