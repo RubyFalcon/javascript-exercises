03d29dd5ffaeae40a0f035e281d6d0284a33a962
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-28 16:08:56 +0000

    create palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedWords = words
+    .toLowerCase()
+    .split("")
+    .filter((letter)=> alphanumerical.includes(letter))
+    .join("");
 
+    const  reversedWords = cleanedWords.split("").reverse().join("")
+    return cleanedWords === reversedWords;
 };
 
 // Do not edit below this line
