a677ad42bc38f38e22276390b07691fbf8e9d753
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-28 14:59:51 +0000

    create calculator functionalilty

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+ b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0 ){
+    return 0;
+  }
+  else if (arr.length <2){
+
+    return arr[0];
+  }
+	else{
+    return arr.reduce((start, next)=> start+next, 0) 
+  }
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(arr.length == 0 ){
+    return 0;
+  }
+  else if (arr.length <2){
 
+    return arr[0];
+  }
+	else{
+    return arr.reduce((start, next)=> start*next, 1) 
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let factorial = 1;
+    for (let  i = 1; i <= n; i++){
+        factorial*= i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
