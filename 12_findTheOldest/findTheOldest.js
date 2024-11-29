3ef372e61ac5d5eaa0d075b80d37d2f59e261cf0
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-29 13:57:05 +0000

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+const findTheOldest = function(people) {
+    const oldestToYoungest = people.sort((a,b)=> {
+        
+        const firstGuy = getAge(a.yearOfBirth, a.yearOfDeath)
+        const secondGuy = getAge(b.yearOfBirth, b.yearOfDeath);
+        return firstGuy > secondGuy ? -1 : 1;
+    })
+    return oldestToYoungest[0];
 };
 
 // Do not edit below this line
